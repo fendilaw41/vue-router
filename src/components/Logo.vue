de52ea21853a26b4cf321ae2a5a4c04401d000e2
fendilaw41 <fendilaw41@gmail.com>
2021-05-06 09:41:20 +0700

    fendi

---
+++ src/components/Logo.vue
@@ -0,0 +1,46 @@
+<template>
+        <div class="item-fendi">
+              <img class="logo" src="../assets/instagram.png" alt="instagram">
+          </div>
+          <div class="item-fendi">
+              <img class="logo" src="../assets/vuejs.svg" alt="Google">
+          </div>
+          <div class="item-fendi">
+              <img class="logo" src="../assets/nuxt.svg" alt="nuxt">
+          </div>
+          <div class="item-fendi">
+              <img class="logo" src="../assets/instagram.png" alt="instagram">
+          </div>
+          <div class="item-fendi">
+              <img class="logo" src="../assets/vuejs.svg" alt="Google">
+          </div>
+          <div class="item-fendi">
+              <img class="logo" src="../assets/nuxt.svg" alt="nuxt">
+          </div>
+          <div class="item-fendi">
+              <img class="logo" src="../assets/vuejs.svg" alt="Google">
+          </div>
+          <div class="item-fendi">
+              <img class="logo" src="../assets/nuxt.svg" alt="nuxt">
+          </div>
+</template>
+
+<script>
+export default {
+ name: 'Logo',
+}
+</script>
+
+<style scoped>
+.item-fendi {
+    float: left;
+    margin-right: 70px;
+    margin-bottom: 70px;
+    width: 150px;
+    height: 150px;
+  }
+  .item-fendi .logo{
+        width: 150px;
+        height: 150px;
+      }
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="profile">
+     <h1>This is an Profile page</h1>
+  </div>
+  <!-- <Logo /> -->
+</template>
+
+<script>
+// import Logo from ".components/Logo.vue";
+
+export default {
+  name: 'profile',
+
+  components: {
+  //  Logo,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
